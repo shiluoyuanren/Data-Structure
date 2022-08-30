ccee590da2eb9a2f0396fa3d4ee17bf44e55ed72
shiluoyuanren <1077598752@qq.com>
2022-08-30 23:16:56 +0800

    version 2.0(添加了一个测试)

--- Algorithms/STL/vector/src/main.cpp
+++ Algorithms/STL/vector/src/main.cpp
@@ -21,3 +21,23 @@ int main() {
     system("pause");
     return 0;
 }
+
+//对vector用法的一些测试
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     int i;
+//     vector<int>* q = new vector<int>;
+//     !!!此时必须申请一个数组的空间,否则无法用push_back等函数
+//     for (i = 0; i < 5; ++i) {
+//         q->push_back(i);
+//         cout << q->at(i) << " "; at()函数用来访问元素,list不可以用
+//     }
+//     cout << endl;
+//     delete q;   !!!注意此时不是delete[] q
+
+//     system("pause");
+//     return 0;
+// }
